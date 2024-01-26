6794c419fdd137a56f5aff13f0e98ec9a6426956
Satyam Kumar <satyam.kumar1@ibm.com>
2024-01-26 21:34:48 +0530

    eshop product and home screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,16 @@
 import React from 'react'
+import { Outlet } from 'react-router-dom'
 import { Container } from 'react-bootstrap'
 import Header from './components/Header'
 import Footer from './components/Footer'
-import HomeScreen from './screens/HomeScreen'
+
 const App = () => {
   return (
     <>
       <Header />
       <main className='py-3'>
         <Container>
-          <HomeScreen/>
+          <Outlet/>
         </Container>
       </main>
       <Footer/>
