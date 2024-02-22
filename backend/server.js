1a2a59b6ea2a003371afaafb9622d02cf2ec01bd
satyamjha22 <satyam22oct@gmail.com>
2024-02-22 15:07:33 +0530

    backend Authentication, jwt, login logout,

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv'
  dotenv.config()
+ import cookieParser from 'cookie-parser';
  import connectDB from './config/db.js';
  import userRoutes from './routes/userRoutes.js';
  import {notFound, errorHandler } from './middleware/errorMiddleware.js';
@@ -13,6 +14,7 @@ const app = express()
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.get('/', (req, res) => {
     res.send('API is running....');
