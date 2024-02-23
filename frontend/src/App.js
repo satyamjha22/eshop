aeee1bd0696818e90bb39e6fd27bde6a5b660b03
satyamjha22 <satyam22oct@gmail.com>
2024-02-23 14:04:08 +0530

    front end Authentication implemented (login, logout, new user register)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,17 +3,20 @@ import { Outlet } from 'react-router-dom'
 import { Container } from 'react-bootstrap'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
   return (
     <>
+      <ToastContainer />
       <Header />
       <main className='py-3'>
         <Container>
-          <Outlet/>
+          <Outlet />
         </Container>
       </main>
-      <Footer/>
+      <Footer />
     </>
   )
 }
